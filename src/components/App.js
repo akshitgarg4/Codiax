14f98b371893c6c8b8b3149fc001346d434ae5b6
akshitgarg4 <akshit2000garg@gmail.com>
2020-07-01 14:10:39 +0530

    added first reducer of getting posts from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/posts';
 
 class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
   render() {
     console.log(this.props);
     return <div>App</div>;
