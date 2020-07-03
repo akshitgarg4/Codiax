8ec84363f062f298a7807bd86d1680fec5d0d18c
akshitgarg4 <akshit2000garg@gmail.com>
2020-07-03 14:24:53 +0530

    router added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
 import PostList from './PostList';
 import Nav from './Nav';
 
+const signup = () => {
+  return <div>Sign up</div>;
+};
+const login = () => {
+  return <div>Login</div>;
+};
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -14,16 +21,18 @@ class App extends React.Component {
     const { posts } = this.props;
     console.log(this.props);
 
-    
     return (
-      <div>
-        {/*Navbar */}
-        <Nav/>
+      <Router>
+        <div>
+          {/*Navbar */}
+          <Nav />
+          <Route path="/login" component={login} />
+          <Route path="/signup" component={signup} />
 
-
-        {/*posts  list*/}
-        <PostList posts={posts} />
-      </div>
+          {/*posts  list*/}
+          <PostList posts={posts} />
+        </div>
+      </Router>
     );
   }
 }
