21328f99cb1fdbf2d1787888ebabdadbfbfa885d
akshitgarg4 <akshit2000garg@gmail.com>
2020-07-05 12:14:47 +0530

    user profile added

--- src/components/App.js
+++ src/components/App.js
@@ -9,26 +9,25 @@ import Home from './Home';
 import Page404 from './Page404';
 import Nav from './Nav';
 import settings from './Settings';
+import UserProfile from './UserProfile';
 import Login from './Login';
 import SignUp from './Signup';
 import * as jwtDecode from 'jwt-decode';
 import { authenticateUser } from '../actions/auth';
 
-
-
-  
 //creating own private routes
-const PrivateRoute = (privateProps) =>{
-  const {isLoggedIn,path,component:Component} = privateProps;
-  return(
-    <Route path={path} render={(props)=>{
-      return isLoggedIn ? <Component {...props} /> : <Redirect to='/login'/>
-    }}/>
+const PrivateRoute = (privateProps) => {
+  const { isLoggedIn, path, component: Component } = privateProps;
+  return (
+    <Route
+      path={path}
+      render={(props) => {
+        return isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />;
+      }}
+    />
   );
 };
 class App extends React.Component {
-
-  
   componentDidMount() {
     this.props.dispatch(fetchPosts());
     const token = localStorage.getItem('token');
@@ -46,10 +45,8 @@ class App extends React.Component {
     }
   }
 
-  
-
   render() {
-    const { posts,auth } = this.props;
+    const { posts, auth } = this.props;
     console.log(this.props);
 
     return (
@@ -71,7 +68,16 @@ class App extends React.Component {
             />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={SignUp} />
-            <PrivateRoute path='/settings' component={settings}  isLoggedIn={auth.isLoggedIn} />
+            <PrivateRoute
+              path="/settings"
+              component={settings}
+              isLoggedIn={auth.isLoggedIn}
+            />
+            <PrivateRoute
+              path="/user/:userId"
+              component={UserProfile}
+              isLoggedin={auth.isLoggedin}
+            />
             <Route component={Page404} />
           </Switch>
         </div>
