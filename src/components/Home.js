a8a3b10c7de6f2174d4e4461811e04f6db19a5d5
akshitgarg4 <akshit2000garg@gmail.com>
2020-07-06 18:20:02 +0530

    chat ui added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 
 import PostList from './PostList';
 import FriendsList from './FriendsList';
+import Chat from './Chat';
 
 class Home extends Component {
   render() {
@@ -13,9 +14,10 @@ class Home extends Component {
     return (
       <div className="home">
         {/*posts  list*/}
-      <PostList posts={posts} />
-      {isLoggedIn && <FriendsList friends={friends} />}
-    </div>
+        <PostList posts={posts} />
+        {isLoggedIn && <FriendsList friends={friends} />}
+        {isLoggedIn && <Chat />}
+      </div>
     );
   }
 }
